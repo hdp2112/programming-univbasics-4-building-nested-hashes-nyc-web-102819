e86cbe2305ec6ff401fb98df0f00e5afcaf2176c
Hector Polanco <hdp2112@columbia.edu>
2019-10-16 01:07:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{:name => "Hector"}
+	monopoly = {
+	  :railroads => {"Hector"}
+	  
+	}
 end
 
 def monopoly_with_second_tier
