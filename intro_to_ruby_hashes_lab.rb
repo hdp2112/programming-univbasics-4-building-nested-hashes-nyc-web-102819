95c4b2fd43680e886665bb156c5f8f0fb3c0505e
Hector Polanco <hdp2112@columbia.edu>
2019-10-16 01:14:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,13 @@ def monopoly_with_second_tier
   # complexity.
   
   monopoly = {
-	  :railroads => {:pieces => 4, :rent_in_dollars => []}
+	  :railroads => {
+	    :pieces => 4, 
+	    :rent_in_dollars => {
+	      :one_piece_owned = 
+	    }
+	    
+	  }
 	}
 end
 
