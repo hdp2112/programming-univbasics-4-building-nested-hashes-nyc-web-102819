9d18ca9449865330aff4f27c5bd347eab2ce29f5
Hector Polanco <hdp2112@columbia.edu>
2019-10-16 01:35:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
 	        :four_pieces_owned => 200
 	       },
 	     :names => {
-	       :reading_railroad => {:mortgage_value => ($100).to_s},
+	       :reading_railroad => {:mortgage_value => ("$" + 100.to_s)},
 	       :pennsylvania_railroad => {:mortgage_value => "$200"},
 	       :b_and_o_railroad => {:mortgage_value => "$400"},
 	       :shortline_railroad => {:mortgage_value => "$800"}
